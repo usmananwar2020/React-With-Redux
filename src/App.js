2ec81ea4873e5549c3afbfc83c22ab5318b02420
usmananwar2020 <muhammadusman1531995@mail.com>
2020-08-29 17:43:55 +0500

    remove Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { store } from "./Redux/Store";
 import user from './components/User.css';
 import { Provider } from "react-redux"
@@ -21,11 +21,11 @@ console.log(JSON.parse(getUser));
         <h1>Free Images and Free Stock</h1>
         <>
                 <Router>
-                  <Switch>
+                  {/* <Switch> */}
                   <Route exact path="/" component={Home} />
                   <Route exact path="/images/:name" component={User} />
                   <Route  component={()=><h1>Not Found</h1>} />
-                    </Switch>
+                    {/* </Switch> */}
                 </Router>
             </>
       </Provider>
